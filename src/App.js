590e7e38d940a9b49c3c8d32977e9724c226acc2
GAURAV131X <sonigaurav2207@gmail.com>
2021-01-20 23:25:06 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
+import { Button } from "reactstrap";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/create">Create Survey</Route>
+          <Route path="/take">Take Survey</Route>
+          <Route path="/">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <Link to="/create">
+                <Button className="survey-main-button">Create Survey</Button>
+              </Link>
+              <Link to="/take">
+                <Button className="survey-main-button">Take Survey</Button>
+              </Link>
+            </header>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
